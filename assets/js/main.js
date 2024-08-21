9599ff2cf5c9e7d044452b681d1a9cf132bed0cc
AdrienForbice93 <didead93@gmail.com>
2024-08-21 10:29:21 +0200

    Etudes lvl 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,7 +136,7 @@ resumeButton.addEventListener('click', () =>{
     
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
 
-    // setTimeout(removeScale, 5000)
+    setTimeout(removeScale, 5000)
  
 })
 
